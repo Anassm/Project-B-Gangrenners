a9682bd5a0ce4449d945eb41351a8f26514f891e
Anass Moussadi <a.moussadi03@gmail.com>
2024-10-14 15:50:27 +0200

    Display single showtimes based on their id & Display all showtimes which play a specific movie id.

---
+++ Project/Logic/ShowtimesLogic.cs
@@ -0,0 +1,25 @@
+using System.Dynamic;
+
+public class ShowtimesLogic
+{
+    private List<ShowtimeModel> _showtimes;
+
+    public ShowtimesLogic()
+    {
+        _showtimes = ShowtimesAccess.LoadAll();
+    }
+
+    public List<ShowtimeModel> getShowtimesByMovieId(int movieId)
+    {
+        List<ShowtimeModel> showtimes = _showtimes.FindAll(showtime => showtime.MoviesId == movieId);
+
+        return showtimes;
+    }
+
+    public ShowtimeModel GetShowtimeById(int id)
+    {
+        ShowtimeModel showtime = _showtimes.Find(showtime => showtime.Id == id);
+
+        return showtime;
+    }
+}
