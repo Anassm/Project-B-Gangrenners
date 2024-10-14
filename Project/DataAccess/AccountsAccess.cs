a676f447b093ce6cd92dde56898f003db354f690
RedRoan04 <roanvrossum@gmail.com>
2024-10-14 13:18:48 +0200

    Added comments and checked working

--- Project/DataAccess/AccountsAccess.cs
+++ Project/DataAccess/AccountsAccess.cs
@@ -5,6 +5,7 @@ static class AccountsAccess
     static string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/accounts.json"));
 
 
+    // Load all accounts from the json file
     public static List<AccountModel> LoadAll()
     {
         string json = File.ReadAllText(path);
@@ -12,6 +13,7 @@ static class AccountsAccess
     }
 
 
+    // Write all accounts to the json file
     public static void WriteAll(List<AccountModel> accounts)
     {
         var options = new JsonSerializerOptions { WriteIndented = true };
