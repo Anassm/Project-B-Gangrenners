83053a3421ec5ec65e457b392a4f989de3eabb80
RedRoan04 <roanvrossum@gmail.com>
2024-10-14 14:35:02 +0200

    Added the Access and model file

---
+++ Project/DataModels/ReservationModel.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+public class ReservationModel
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("seatid")]
+    public int SeatId { get; set; }
+
+    [JsonPropertyName("showtimeid")]
+    public int ShowtimeId { get; set; }
+
+    [JsonPropertyName("accountid")]
+    public int AccountId { get; set; }
+
+    [JsonPropertyName("totalprice")]
+    public double TotalPrice { get; set; }
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    public ReservationModel(int id, int seatId, int showtimeId, int accountId, double totalPrice, string code)
+    {
+        Id = id;
+        SeatId = seatId;
+        ShowtimeId = showtimeId;
+        AccountId = accountId;
+        TotalPrice = totalPrice;
+        Code = code;
+    }
+
+}
