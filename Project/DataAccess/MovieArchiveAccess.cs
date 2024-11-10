6f72874358f87a538b492d4015bc0495eed1de2f
jenskon <jens.koning@icloud.com>
2024-11-10 21:05:39 +0100

    access layer

---
+++ Project/DataAccess/MovieArchiveAccess.cs
@@ -0,0 +1,39 @@
+using System.Text.Json;
+
+static class MovieArchiveAccess
+{
+    static string path = System.IO.Path.GetFullPath(System.IO.Path.Combine(Environment.CurrentDirectory, @"DataSources/moviearchive.json"));
+
+
+    public static List<MovieArchiveModel> LoadAll()
+    {
+        try
+        {
+            string json = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<List<MovieArchiveModel>>(json) ?? new List<MovieArchiveModel>();
+        }
+        catch (Exception)
+        {
+            return new List<MovieArchiveModel>();
+        }
+    }
+
+
+    public static void WriteAll(List<MovieArchiveModel> movies)
+    {
+        var options = new JsonSerializerOptions { WriteIndented = true };
+        try 
+        {
+            
+            string json = JsonSerializer.Serialize(movies, options);
+            File.WriteAllText(path, json);
+        }
+        catch (Exception)
+        {
+            return;
+        }
+    }
+
+
+
+}
