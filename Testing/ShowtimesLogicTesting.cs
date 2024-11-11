6bb55fe379a6366881a2e49f850fc8c84e7447c4
RedRoan04 <roanvrossum@gmail.com>
2024-11-11 20:04:39 +0100

    Added some unit tests, updated Showtimes logic

--- Testing/ShowtimesLogicTesting.cs
+++ Testing/ShowtimesLogicTesting.cs
@@ -12,10 +12,63 @@ public class TestShowtimesLogic
     }
 
     [TestMethod]
-    public void GetShowtimeByIIId_ExistingShowtime_ReturnShowtime()
+    public void GetShowTimesByMovieId_NonExistingMovies_ReturnEmptyList()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+
+        Assert.AreEqual(showtimesLogic.GetShowtimesByMovieId(0).Count, 0);
+    }
+
+    [TestMethod]
+    public void GetShowtimeById_ExistingShowtime_ReturnShowtime()
     {
         ShowtimesLogic showtimesLogic = new ShowtimesLogic();
 
         Assert.AreEqual(showtimesLogic.GetShowtimeById(1).Id, 1);
+    }
+
+    [TestMethod]
+    public void GetShowtimeById_NonExistingShowtime_ReturnNull()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+
+        Assert.IsNull(showtimesLogic.GetShowtimeById(0));
+    }
+
+    [TestMethod]
+    public void AddShowTime_ValidShowtime_AddShowtime()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+        ShowtimeModel showtime = new ShowtimeModel(4, 1, DateTime.Now, 1, new int[10,10]);
+
+        showtimesLogic.AddShowtime(showtime);
+
+        Assert.IsNotNull(showtimesLogic.GetShowtimeById(4));
+    }
+
+    [TestMethod]
+    public void RemoveShowtime_ExistingShowtime_RemoveShowtime()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+
+        showtimesLogic.RemoveShowtime(1);
+
+        Assert.AreEqual(null, showtimesLogic.GetShowtimeById(1));
+    }
+
+    [TestMethod]
+    public void ReserveSeat_ExistingShowtimeAndSeat_ReserveSeat()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+
+        Assert.IsTrue(showtimesLogic.ReserveSeat(2, 8, 8));
+    }
+
+    [TestMethod]
+    public void ReserveSeat_NonExistingShowtimeAndSeat_DoNothing()
+    {
+        ShowtimesLogic showtimesLogic = new ShowtimesLogic();
+
+        Assert.IsFalse(showtimesLogic.ReserveSeat(0, 0, 0));
     }
 }
