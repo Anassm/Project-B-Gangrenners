7d98bf74f4ffc77e848d8a8067f3f3622bfffa18
RedRoan04 <roanvrossum@gmail.com>
2024-11-20 15:04:21 +0100

    Updated logic layer

--- Project/Presentation/ManageMovies.cs
+++ Project/Presentation/ManageMovies.cs
@@ -209,11 +209,7 @@ public static class ManageMovies
         System.Console.WriteLine("---------------------------------------");
         foreach (MovieModel movie in MoviesArchiveLogic._movies)
         {
-            System.Console.WriteLine($"ID: {movie.Id}");
-            System.Console.WriteLine($"Name: {movie.Name}");
-            System.Console.WriteLine($"Genre: {movie.Genre}");
-            System.Console.WriteLine($"Duration: {movie.Duration}");
-            System.Console.WriteLine($"Promoted: {movie.Promoted}");
+            System.Console.WriteLine(movie.ToStringComplete());
             System.Console.WriteLine("---------------------------------------");
         }
         System.Console.WriteLine("Archive list shown above.");
@@ -242,11 +238,7 @@ public static class ManageMovies
         System.Console.WriteLine("---------------------------------------");
         foreach (MovieModel movie in MoviesLogic._movies)
         {
-            System.Console.WriteLine($"ID: {movie.Id}");
-            System.Console.WriteLine($"Name: {movie.Name}");
-            System.Console.WriteLine($"Genre: {movie.Genre}");
-            System.Console.WriteLine($"Duration: {movie.Duration}");
-            System.Console.WriteLine($"Promoted: {movie.Promoted}");
+            System.Console.WriteLine(movie.ToStringComplete());
             System.Console.WriteLine("---------------------------------------");
         }
         System.Console.WriteLine("current movie list shown above.");
