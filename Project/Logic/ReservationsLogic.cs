2e0eae0ce4496ce96bbf6fe21f4166734bec587c
jenskon <jens.koning@icloud.com>
2024-11-25 14:27:14 +0100

    see reservations presentation layer

--- Project/Logic/ReservationsLogic.cs
+++ Project/Logic/ReservationsLogic.cs
@@ -79,7 +79,7 @@ public class ReservationsLogic
         return _reservations.Count == 0 ? 1 : _reservations.Max(s => s.Id) + 1;
     }
 
-    public List<ReservationModel> SeeFutureReservations(int accountid)
+    public static  List<ReservationModel> SeeFutureReservations(int accountid)
     {
         List<ReservationModel> reservations = [];
         foreach (ReservationModel reserv in _reservations)
@@ -92,12 +92,12 @@ public class ReservationsLogic
         return reservations;
     }
 
-    public List<ReservationModel> SeePastReservations(int accountid)
+    public static List<ReservationModel> SeePastReservations(int accountid)
     {
         List<ReservationModel> reservations = [];
         foreach (ReservationModel reserv in _reservations)
         {
-            if (reserv.AccountId == accountid && DateTime.Now.CompareTo(ShowtimesLogic.GetShowtimeById(reserv.ShowtimeId).Time) = -1)
+            if (reserv.AccountId == accountid && (DateTime.Now.CompareTo(ShowtimesLogic.GetShowtimeById(reserv.ShowtimeId).Time) == -1))
             {
                 reservations.Add(reserv);
             }
