580487a15184383ce00a5aa4d930a6487a511d49
Anass Moussadi <a.moussadi03@gmail.com>
2024-12-17 19:10:33 +0100

    Logic layer subscriptions add and remove subscription methods

---
+++ Project/Logic/SubscriptionLogic.cs
@@ -0,0 +1,34 @@
+using System.Data.Common;
+
+public class SubscriptionLogic
+{
+    static private List<SubscriptionModel> _subscriptions { get; set; } = SubscriptionAccess.LoadAll();
+
+    public void AddSubscription(int userId)
+    {
+        int id = _subscriptions.Count + 1;
+        string name = "Loyal";
+        int membershipNumber = int.Parse(DateTime.Now.Year.ToString() + userId.ToString());
+        int views = 15;
+        DateTime startDate = DateTime.Now;
+
+        SubscriptionModel subscription = new SubscriptionModel(id, userId, name, membershipNumber, views, startDate);
+
+        _subscriptions.Add(subscription);
+        SubscriptionAccess.WriteAll(_subscriptions);
+    }
+
+    public bool RemoveSubscription(int userId)
+    {
+        SubscriptionModel? subscription = _subscriptions.Find(sub => sub.UserId == userId);
+        if (subscription == null)
+        {
+            return false;
+        }
+
+        _subscriptions.Remove(subscription);
+        SubscriptionAccess.WriteAll(_subscriptions);
+
+        return true;
+    }
+}
