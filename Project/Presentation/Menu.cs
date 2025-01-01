d07a221228c5043780da5f011bcd2c122e3f7e71
jenskon <jens.koning@icloud.com>
2025-01-01 12:12:47 +0100

    Menu visual experience updated

--- Project/Presentation/Menu.cs
+++ Project/Presentation/Menu.cs
@@ -12,52 +12,11 @@ static class Menu
 
     static public void Start()
     {
+        AccountsLogic.CurrentAccount = null;
         string StartMessage = "Welcome to this Cinema";
         string[] MenuNames = {"Login", "Register", "Continue as quest", "Login as admin", "Login as accountant", "About page", "Exit"};
         Action[] Actions = {UserLogin.Start, Register.Start, MainMenu, AdminLogin.Start, AccountantLogin.Start, AboutPage.Start, Exit,};
         SelectingMenu.MenusSelect(MenuNames, Actions, StartMessage);
-
-        //Implement logic to show different menus depending on the user's role
-
-        //This is the main menu
-    //     Console.WriteLine("Enter 1 to login");
-    //     Console.WriteLine("Enter 2 to register");
-    //     Console.WriteLine("Enter 3 to continue as guest");
-    //     Console.WriteLine("Enter 4 to login as admin");
-    //     Console.WriteLine("Enter 5 to login as accountant");
-    //     Console.WriteLine("Enter 6 to go to our about page");
-    //     Console.WriteLine("Enter 7 to exit");
-
-    //     string input = Console.ReadLine();
-    //     Console.Clear();
-    //     switch (input)
-    //     {
-    //         case "1":
-    //             UserLogin.Start();
-    //             break;
-    //         case "2":
-    //             Register.Start();
-    //             break;
-    //         case "3":
-    //             MainMenu();
-    //             break;
-    //         case "4":
-    //             AdminLogin.Start();
-    //             break;
-    //         case "5":
-    //             AccountantLogin.Start();
-    //             break;
-    //         case "6":
-    //             AboutPage.Start();
-    //             break;
-    //         case "7":
-    //             Exit();
-    //             break;
-    //         default:
-    //             Console.WriteLine("Invalid input");
-    //             Start();
-    //             break;
-    //     }
     }
 
     public static void Exit()
@@ -70,48 +29,17 @@ static class Menu
     {
         if (AccountsLogic.CurrentAccount != null)
         {
-            Console.Clear();
-            Console.WriteLine("Enter 1 to search movies by date and buy ticket");
-            Console.WriteLine("Enter 2 to search all movies and buy ticket");
-            Console.WriteLine("Enter 3 to see reservation");
-            Console.WriteLine("Enter 4 to logout");
+            string StartMessage = "";
+            string[] MenuNames = {"Search movies by date and buy ticket", "Search all movies and buy ticket", "See reservations", "Logout",};
+            Action[] Actions = {MovieSearch.SearchByDate, MovieSearch.SearchAll, SeeReservations.SeeReservationSubMenu, Start};
+            SelectingMenu.MenusSelect(MenuNames, Actions, StartMessage);
         }
         else
         {
-            Console.Clear();
-            Console.WriteLine("Enter 1 to search movies by date and buy ticket");
-            Console.WriteLine("Enter 2 to search all movies and buy ticket");
-            Console.WriteLine("Enter 3 to register");
-            Console.WriteLine("Enter 4 to go back to main menu");
-        }
-        string input = Console.ReadLine();
-        Console.Clear();
-        switch (input)
-        {
-            case "1":
-                MovieSearch.SearchByDate();
-                break;
-            case "2":
-                MovieSearch.SearchAll();
-                break;
-            case "3":
-                if (AccountsLogic.CurrentAccount != null)
-                {
-                    SeeReservations.SeeReservationSubMenu();
-                }
-                else
-                {
-                    Register.Start();
-                }
-                break;
-            case "4":
-                AccountsLogic.CurrentAccount = null;
-                Start();
-                break;
-            default:
-                Console.WriteLine("Invalid input");
-                MainMenu();
-                break;
+            string StartMessage = "";
+            string[] MenuNames = {"Search movies by date and buy ticket", "Search all movies and buy ticket", "Register", "Go back",};
+            Action[] Actions = {MovieSearch.SearchByDate, MovieSearch.SearchAll, Register.Start, Start};
+            SelectingMenu.MenusSelect(MenuNames, Actions, StartMessage);
         }
     }
 }
